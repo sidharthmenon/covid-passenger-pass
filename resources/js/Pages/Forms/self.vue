13032313d23b22aa5fe625a2be976b58b71bb124
sidharth <sidharth@startupmission.in>
2020-03-04 20:44:14 +0530

    changes

--- resources/js/Pages/Forms/self.vue
+++ resources/js/Pages/Forms/self.vue
@@ -52,7 +52,7 @@
                       .col-6
                         .mb-3
                           label.form-label State
-                          select.form-select(name='state' v-model="form.state" :settings="{ placeholder: 'State' }")
+                          selectize.form-select(name='state' v-model="form.state" :settings="{ placeholder: 'State' }")
                             option(v-for="state in states" :value='state.name') {{state.name}}
                       .col-6
                         form_input(type="text", name="pin", label="PIN", placeholder="PIN" v-model="form.pin")
@@ -74,7 +74,7 @@
                   .col-sm-12
                     .mb-3
                       label.form-label Details of the countries visited since last 28 days?
-                      select.form-select(name='countries' v-model="form.countries" :settings="{ placeholder: 'Details of the countries visited since last 28 days?', maxItems: 15, plugins: ['remove_button'], }")
+                      selectize.form-select(name='countries' v-model="form.countries" :settings="{ placeholder: 'Details of the countries visited since last 28 days?', maxItems: 15, plugins: ['remove_button'], }")
                         option(v-for="country in countries" :value='country.id') {{country.name}}
                     form_input(type="text", name="cities", label="Details of the cities visited since last 28 days?", placeholder="Details of the cities visited since last 28 days?" v-model="form.cities")
                     .mb-3
